b72e420406e1372200bca9e75fd6d12f74f25af0
Nitin Gupta <nitin171538.cse@chitkara.edu.in>
2019-08-24 13:48:47 +0530

    Price of Product Extracted

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-URL = 'https://www.amazon.de/Dell-Alienware-15-R4-Festplatte/dp/B07D5TM1QK/ref=sr_1_1?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=alienware+laptop&qid=1566632631&s=gateway&sr=8-1'
-
+URL = 'https://www.amazon.in/Dell-Alienware-Ready-Gaming-Notebook/dp/B07D6CMJYS/ref=sr_1_3?crid=128LG608426NP&keywords=alienware+laptops&qid=1566634506&s=gateway&sprefix=Alienware%2Caps%2C407&sr=8-3'
 headers = {"User-Agents": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
 page = requests.get(URL, headers=headers)
@@ -9,5 +8,8 @@ page = requests.get(URL, headers=headers)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 title = soup.find(id="productTitle").get_text()
+price = soup.find(id="priceblock_ourprice").get_text()
+converted_price = price[0:13]
 
-print(title.strip())
+print(converted_price)
+#print(price.strip())
