3d94e81700ee46e461b910c013c5f4637a3aea43
Nitin Gupta <nitin171538.cse@chitkara.edu.in>
2019-09-06 13:14:05 +0530

    First Method Updated

---
+++ README.md
@@ -0,0 +1,24 @@
+Hello There,
+This Code basically tracks the price of the product you want to track
+of AMAZON.
+
+You need two things for it.
+1. Your product's URL, which you can get by opening Amazon and then clicking on your product.
+	Copy the URL and paste it in front of the URL variable.
+
+2. Your user agent.
+	You can get it by simply typing 'My user agent' in google and copy and paste whatever comes up
+	infront of 'User Agent' in get_name_price function.
+
+Now there are two ways you can Use this script :-
+1. You can run it one time and it will check the value for your product.
+
+	For this make the neccessary changes in the given code.
+
+
+2. You can turn it on and it will keep checking it untill the price drops, ofCourse after some time intervals.
+	
+	For this comment the given code and uncomment the already commented code and make necessary changes in it.
+
+
+HAPPY TRACKING :)
--- scrapper.py
+++ scrapper.py
@@ -2,18 +2,20 @@ import requests
 from lxml import html
 import smtplib  
 
+#Method 1 of using this code
+
 def sendMail():
 	print("Function called")
 	s = smtplib.SMTP('smtp.gmail.com', 587) 
 	s.starttls() 
-	s.login("hack7jack@gmail.com", "downloadapp007") 
-	message = "Price Dropped Bro"
-	s.sendmail("hack7jack@gmail.com", "gupta7nitin@gmail.com", message) 
+	s.login("senders_email", "password") 						#Your username and password for gmail using which you want to send the mail
+	message = "Price Dropped Bro" 							#Feel free to change the message Dude
+	s.sendmail("senders_email", "receivers_email", message) 	#Sender's Email (mentioned Above) and Receiver's Email (Email on which you want to get the message)
 	s.quit()
 
 
 def get_name_price(URL):
-	headers = {"User-Agents": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
+	headers = {"User-Agents": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'} #Your User Agent
 
 	page = requests.get(URL, headers=headers)
 
@@ -39,7 +41,7 @@ def price_alert(url, dest_price):
 
 	return True
 
-
+#Change the URL according to your product  Mine is Dell-Alienware-Gaming-Laptop!!!!!
 URL = 'https://www.amazon.in/Dell-Alienware-Ready-Gaming-Notebook/dp/B07D6CMJYS/ref=sr_1_3?crid=128LG608426NP&keywords=alienware+laptops&qid=1566634506&s=gateway&sprefix=Alienware%2Caps%2C407&sr=8-3'
 reply = True
 i = 0
@@ -51,3 +53,9 @@ while reply:
 	except Exception as e:
 		print ("Failure to get price from Amazon, trying again")
 		i += 1
+
+##################################################
+
+# Uncomment this to get 2nd use of this script that is keep a constant check.
+
+# SORRY Currently unavailable but surely I ll update it soon
